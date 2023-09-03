3f91bcb48adb095f67497738e5851580ab12a444
M-Loyola <m.yakit.loyola@gmail.com>
2023-09-03 11:14:43 +0800

    feat: add configuration for api fetch data in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./css/TodoList.css";
+import { api } from "../api/api";
 export const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await api;
+            // const response = await api.getTodoTasks();
+            // dispatch(resetTodoList(response.data));
+        }
+        fetchData();
+        console.log(fetchData);
+    }, [])
     return (
         <>
             <div>This is a home page test tes test</div>
