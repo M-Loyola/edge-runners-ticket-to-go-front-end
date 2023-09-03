387fac446ba2310fbe20cd4d5917a78737cc0f90
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-09-03 12:50:27 +0800

    fix: config

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const api = axios.create(
   {
-    qa: {
-      baseURL: "https://ex-ticket-to-go-back-end-qa.up.railway.app/",
-    },
-  }[process.env.REACT_APP_ENV]
+    // qa: {
+    baseURL: "https://ex-ticket-to-go-back-end-qa.up.railway.app/",
+    // },
+  }
+  // [process.env.REACT_APP_ENV]
 );
 export default api;
