7c07957206133705cf6987ed04487f09de818b25
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-09-03 12:14:24 +0800

    set up test: hard code url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const LOCAL_BASE_URL = "http://localhost:8080/";
+const LOCAL_BASE_URL = " http://localhost:8080/";
 const baseURLByEnv = process.env.REACT_APP_BASE_URL || LOCAL_BASE_URL;
 
 export const api = axios.create({
