6a71578795766c9c300b9e0ac334eaa6945cf86d
M-Loyola <m.yakit.loyola@gmail.com>
2023-09-03 12:00:14 +0800

    feat: comment for test in backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,13 @@ import * as apiConfig from "../api/apiConfig"
 export const Home = () => {
     // const dispatch = useDispatch();
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await apiConfig.getMovies();
-            // const response = await api.getTodoTasks();
-            // dispatch(resetTodoList(response.data));
-        }
-        fetchData();
-        console.log(fetchData);
+        // const fetchData = async () => {
+        //     const response = await apiConfig.getMovies();
+        //     // const response = await api.getTodoTasks();
+        //     // dispatch(resetTodoList(response.data));
+        // }
+        // fetchData();
+        // console.log(fetchData);
     }, [])
     return (
         <>
