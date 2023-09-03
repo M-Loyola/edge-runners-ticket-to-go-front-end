b747efdf62aed4c2a47509628d15635d3b909e1f
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-09-03 12:47:29 +0800

    fix: set up react env

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-    'qa': {
-        baseURL: 'https://ex-ticket-to-go-back-end-qa.up.railway.app/'
-    }
-}[process.env.REACT_APP_ENV]);
+const api = axios.create(
+  {
+    qa: {
+      baseURL: "https://ex-ticket-to-go-back-end-qa.up.railway.app/",
+    },
+  }[process.env.REACT_APP_ENV]
+);
 export default api;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
-import * as apiConfig from "../api/apiConfig"
+import * as apiConfig from "../api/apiConfig";
+import { useDispatch } from "react-redux";
 export const Home = () => {
-    // const dispatch = useDispatch();
-    useEffect(() => {
-        // const fetchData = async () => {
-        //     const response = await apiConfig.getMovies();
-        //     // const response = await api.getTodoTasks();
-        //     // dispatch(resetTodoList(response.data));
-        // }
-        // fetchData();
-        // console.log(fetchData);
-    }, [])
-    return (
-        <>
-            <div>This is a home page test tes test</div>
-        </>
-    );
-}
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await apiConfig.getMovies();
+      // const response = await api.getTodoTasks();
+      // dispatch(resetTodoList(response.data));
+    };
+    fetchData();
+    console.log(fetchData);
+  }, []);
+  return (
+    <>
+      <div>This is a home page test tes test</div>
+    </>
+  );
+};
