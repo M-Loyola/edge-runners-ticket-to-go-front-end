48e645882eadd41af481e2e0f37b463cfeb660e9
TACULMA2 <marigold.taculog@oocl.com>
2023-09-04 20:27:02 +0800

    feat: modify NetWeekRelease component, added new component Slide component and remove unused components

--- src/components/NextWeekRelease.js
+++ src/components/NextWeekRelease.js
@@ -1,24 +1,67 @@
-import {motion} from "framer-motion";
-import { useRef, useEffect, useState } from "react";
-import posters from "../posters/posters";
-export const NextWeekRelease = () => {
-return (
-    <>
-<div>
-    <motion.div className="carousel">
-        <motion.div className="inner-carousel">
-            {/* <img src="" alt=""/> */}
-            {posters.map(poster => {
-                return(
-                    <motion.div className="item">
-                        <img src={poster} alt="" />
-                        </motion.div>
-                )
-            })}
-        </motion.div>
-    </motion.div>
-</div>
-</>
-);
-};
+import React, { useEffect } from "react";
+import { StackedCarousel, ResponsiveContainer} from "react-stacked-center-carousel";
+import "../css/Slide.css";
+import { Slide } from "./Slide";
 
+
+const data = [
+    {
+      image: "https://drive.google.com/uc?export=view&id=1eh7EchMv_StrywlmyNZqxOTwlSfEbB94",
+    },
+    {
+      image: "https://drive.google.com/uc?export=view&id=1z5Af__DAlXjKD-KTQLwPnUu9gAdG4am6",
+    },
+    {
+      image: "https://drive.google.com/uc?export=view&id=1IvO_A0v4jBZOZb-yGCdPusW9cQJy-5Ma",
+    },
+    {
+      image: "https://drive.google.com/uc?export=view&id=1mTxKPskOLuhhQzPRmPENkFFUNgQCzzu2",
+    },
+    {
+      image: "https://drive.google.com/uc?export=view&id=1ZatEQ02aIVKbaZ3Tw-G-_FSEraIgkjL1",
+    },
+  ];
+
+export const NextWeekRelease = () => {
+  const ref = React.useRef(StackedCarousel);
+
+  useEffect(() => {
+    setInterval(stuff, 100000);
+    console.log();
+  }, []);
+
+  function stuff() {
+    ref.current?.goNext();
+  }
+
+  return (
+    <>
+    <div className="carousel-banner">
+        <span className="banner-text">Unlocking Next Week's Release!</span>
+      </div>
+    <div className="card">
+      <div style={{ width: "100%", position: "relative" }}>
+        <ResponsiveContainer
+          carouselRef={ref}
+          render={(width, carouselRef) => {
+            return (
+              <StackedCarousel
+                ref={carouselRef}
+                slideComponent={Slide}
+                slideWidth={500}
+                carouselWidth={width}
+                data={data}
+                maxVisibleSlide={3}
+                disableSwipe
+                // customScales={[1, 0.85, 0.7, 0.55]}
+                transitionTime={450}
+              />
+            );
+          }}
+        />
+      
+    </div>
+    </div>
+    </>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ export const Home = () => {
   }, []);
   return (
     <>
-      <div>This is a home page test tes test</div>
       <NextWeekRelease />
     </>
   );
