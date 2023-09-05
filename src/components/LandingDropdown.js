ca66fb4536fbc042817d4e6d2d9ac5f240e342bf
ninajairacagomoc <jairalael101@gmail.com>
2023-09-05 15:10:21 +0800

    feat: installed react select and change the css

--- src/components/LandingDropdown.js
+++ src/components/LandingDropdown.js
@@ -1,19 +1,47 @@
-import React, { useState } from "react";
 import "../assets/styles/LandingDropdown.css";
-const LandingDropdown = () => {
-    const [selectedValue, setSelectedValue] = useState("Manila");
+import Select from "react-select";
+const customStyle = {
+    control: (provided, state) => ({
+        ...provided,
+        backgroundColor: "#D2DE32",
+        border: "1px solid #D2DE32",
+        color: "#333",
+        fontWeight: "bold",
+        padding: 0,
+        cursor: "pointer",
+        borderRadius: 7,
+        
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        padding: 12,
+        backgroundColor: state.isSelected ? "#D9D9D9" : "#fff",
+    }),
+};
 
-    const handleSelectChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
+const location = [
+    {
+        value: "Manila",
+        label: "Manila"
+    },
+    {
+        value: "Ortigas",
+        label: "Ortigas"
+    },
+    {
+        value: "Laguna",
+        label: "Laguna"
+    }
+];
+
+const LandingDropdown = () => {
 
     return (
         <div>
-            <select value={selectedValue} onChange={handleSelectChange} className="select-container" > 
-                <option value="Manila">Manila</option>
-                <option value="Ortigas">Ortigas</option>
-                <option value="Laguna">Laguna</option>
-            </select>
+            <Select className="select-container"
+                options={location}
+                defaultValue={location[0]}
+                styles={customStyle} />
         </div>
     );
 };
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Col, Row } from "antd";
 import { NavLink } from "react-router-dom";
 import movieData from "../assets/data/movieData";
