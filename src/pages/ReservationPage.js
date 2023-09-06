68ecfb26ec5c5c602af382f530210b82dda8e439
M-Loyola <m.yakit.loyola@gmail.com>
2023-09-06 15:42:38 +0800

    feat: add card detalis for reservation details

--- src/pages/ReservationPage.js
+++ src/pages/ReservationPage.js
@@ -1,92 +1,77 @@
-import React, { useState } from "react";
-import { Table, Button, Modal } from 'antd';
+import { Button, Card, Row, Col } from 'antd';
 
-
-
-const data = [
-    {
-      key: '1',
-      title: 'Bumblebee',
-      description: "Lorem ipsum just john wick things",
-      location: 'Manila/Cinema One',
-      duration: 140,
+const reservationDetails = [
+  {
+    key: '1',
+    title: 'Bumblebee',
+    description: "Lorem ipsum just john wick things",
+    location: 'Manila/Cinema One',
+    duration: 140,
     schedule: "2023-09-04 10:30:00",
     reservedSeats: "A1,A2",
+    phoneNumber: "09569453524",
     location: "Laguna",
     price: 240,
     cinemaName: "Sm Sta rosa Cinema 1",
-      action: (
-        <>
-          <Button type="primary">Reserve Now!</Button>
-          <Button type="primary">Pay Now!</Button>
-        </>
-      ),
-    },
-  ];
-  
-  const columns = [
-    {
-      title: 'Movie',
-      dataIndex: 'movie',
-      key: 'movie',
-    },
-    {
-      title: 'Location',
-      dataIndex: 'location',
-      key: 'location',
-    },
-    {
-      title: 'Schedule',
-      dataIndex: 'schedule',
-      key: 'schedule',
-    },
-    {
-      title: 'Online Payment',
-      dataIndex: 'onlinePayment',
-      key: 'onlinePayment',
-    },
-    {
-      title: 'Payment Number',
-      dataIndex: 'paymentNumber',
-      key: 'paymentNumber',
-    },
-    {
-      title: 'Seat',
-      dataIndex: 'seat',
-      key: 'seat',
-    },
-    {
-      title: 'Ticket Price',
-      dataIndex: 'ticketPrice',
-      key: 'ticketPrice',
-    },
-    {
-      title: 'Total Price',
-      dataIndex: 'totalPrice',
-      key: 'totalPrice',
-    },
-    {
-      title: 'Actions',
-      dataIndex: 'action',
-      key: 'action',
-    },
-  ];
+    action: (
+      <>
+        <Button type="primary">Reserve Now!</Button>
+        <Button type="primary">Pay Now!</Button>
+      </>
+    ),
+  },
+];
 
 const ReservationPage = () => {
-    
-
-      return (
-        <div>
-        <Table
-          columns={columns}
-          dataSource={data}
-          pagination={false}
-          size="middle"
-          scroll={{ x: 'max-content' }}
-        />
-        
-        </div>
-      );
-    };
+  // reservationDetails.map((details) => console.log(details))
+  return (
+    <div>
+      <Card
+        title="Card title"
+        bordered={false}
+        style={{
+          width: 300,
+        }}
+      >
+        {
+          reservationDetails.map((details) => {
+            (
+              <>
+                <Row>
+                  <Col>Movie Title</Col>
+                  <Col>{details.title}</Col>
+                </Row>
+                <Row>
+                  <Col>Location</Col>
+                  <Col>{details.location}</Col>
+                </Row>
+                <Row>
+                  <Col>Location</Col>
+                  <Col>{details.location}</Col>
+                </Row>
+                <Row>
+                  <Col>Online Payment</Col>
+                  <Col>GCash</Col>
+                </Row>
+                <Row>
+                  <Col>Payment Number</Col>
+                  <Col>{details.phoneNumber}</Col>
+                </Row>
+                <Row>
+                  <Col>Ticket Price</Col>
+                  <Col>{details.price}</Col>
+                </Row>
+                <Row>
+                  <Col>Total Price</Col>
+                  <Col>{details.price*3}</Col>
+                </Row>
+              </>
+            )
+          })
+        }
+      </Card>
+    </div>
+  );
+};
 
 export default ReservationPage;
