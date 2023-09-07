cd789d9be2f3b6389d34915a8d891fb44c7376b2
ninajairacagomoc <jairalael101@gmail.com>
2023-09-07 15:26:41 +0800

    feat:added the movie details reservation

--- src/components/ReservatationStatus.js
+++ src/components/ReservatationStatus.js
@@ -9,7 +9,15 @@ const reservationData = [
         date: "09/03/23",
         cinemaLocation: "SM Manila",
         schedule: "1:00 PM to 3:00 PM",
-        status: "Confirmed",
+        status: "Reserved",
+        imageUrl: "https://i.pinimg.com/originals/5f/1c/3f/5f1c3f76297204de2346b0bfce854000.jpg",
+    },
+    {
+        movieTitle: "Bumblebee",
+        date: "09/03/23",
+        cinemaLocation: "SM Manila",
+        schedule: "1:00 PM to 3:00 PM",
+        status: "Reserved",
         imageUrl: "https://i.pinimg.com/originals/5f/1c/3f/5f1c3f76297204de2346b0bfce854000.jpg",
     }
 ];
@@ -18,16 +26,28 @@ export const ReservationStatus = () => {
     return (
         <div>
             <h3>RESERVATION : </h3>
-            <Row gutter={[16, 16]}> {reservationData.map((reservation, index) => (
-            
+            <Row>
+                <Row gutter={[0, 12]}> {reservationData.map((reservation, index) => (
             <React.Fragment key={index}>
                 <Col span={12}>
                     <div className="reservation-card">
                         <img src={reservation.imageUrl} alt={reservation.movieTitle} />
                     </div>
                 </Col>
+                <Col span={12}>
+                        <div className="reservation-details">
+                            <Text>
+                                Movie Title: {reservation.movieTitle}<br />
+                                Date: {reservation.date}<br />
+                                Cinema Location: {reservation.cinemaLocation}<br />
+                                Schedule: {reservation.schedule}<br />
+                                Status: {reservation.status}<br />
+                            </Text>
+                        </div>
+                    </Col>
             </React.Fragment>))}
         </Row>
+            </Row>
         </div>
     );
 };
