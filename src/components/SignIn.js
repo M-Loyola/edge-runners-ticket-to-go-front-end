945eada876b5a1ac14d00c1177c17f9099bb12bc
TACULMA2 <marigold.taculog@oocl.com>
2023-09-07 19:59:48 +0800

    feat: added toggleview on password in sigin in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,18 +4,32 @@ import { useNavigate } from "react-router-dom";
 import { signIn } from "../api/apiConfig";
 import "../assets/styles/AccountPage.css";
 import { setLoggedInUser } from "../reducers/ticketReducer";
+import eye from '../assets/icons/eye-regular.svg';
+import eyeslash from '../assets/icons/eye-slash-regular.svg';
+import { Alert } from 'antd';
 
 export const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showEmailAlert, setShowEmailAlert] = useState(false);
+
   const handleEmailOnChange = (e) => {
-    setEmail(e.target.value);
+    const {value} = e.target;
+    setEmail(value);
+        const isValidEmail = value.includes('@') && value.includes('.com');
+        setShowEmailAlert(!isValidEmail);
   };
   const handlePasswordOnChange = (e) => {
     setPassword(e.target.value);
   };
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const handleBack = async () => {
     const signInData = {
       email: email,
@@ -43,20 +57,31 @@ export const SignIn = () => {
           placeholder="Email"
           value={email}
           onChange={handleEmailOnChange}
-        />
+        /> 
+        {showEmailAlert && (
+            <Alert message="Invalid email domain!" type="error" showIcon />
+        )}
       </div>
       <div className="form-group">
         <label htmlFor="password">Password: </label>
         <input
-          type="password"
+          type={showPassword ? "text" : "password"}
           placeholder="Password"
           value={password}
           onChange={handlePasswordOnChange}
         />
+        <span className="password-toggle" onClick={togglePasswordVisibility}>
+            <img
+              src={showPassword ? eyeslash : eye}
+              alt = "Toggle Password"
+              className={"eye-icon"}
+            />
+          </span>
       </div>
       <button type="submit" onClick={handleBack}>
         Proceed
       </button>
+
     </>
   );
 };
