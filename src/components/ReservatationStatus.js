5a78d2e11a9bf6a12f95841ac4662610fd557d46
ninajairacagomoc <jairalael101@gmail.com>
2023-09-07 20:09:26 +0800

    feat: added component for reservation status and css in the view reservation page

--- src/components/ReservatationStatus.js
+++ src/components/ReservatationStatus.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Typography, Row, Col } from 'antd';
+import { Typography, Row, Col ,Button} from 'antd';
 import "../assets/styles/ReservationStatus.css";
+import { NavLink } from 'react-router-dom';
 
 
 const { Text } = Typography;
@@ -10,50 +11,40 @@ const reservationData = [
         imageUrl: "https://i.pinimg.com/originals/5f/1c/3f/5f1c3f76297204de2346b0bfce854000.jpg",
         movieTitle: "Bumblebee",
         date: "09/03/23",
-        Location: "SM Manila",
+        location: "SM Manila",
         schedule: "1:00 PM to 3:00 PM",
         status: "Reserved",
     },
-    {
-        imageUrl: "https://i.pinimg.com/originals/5f/1c/3f/5f1c3f76297204de2346b0bfce854000.jpg",
-        movieTitle: "Bumblebee",
-        date: "09/03/23",
-        Location: "SM Manila",
-        schedule: "1:00 PM to 3:00 PM",
-        status: "Reserved",
-    }
 ];
-
 
 export const ReservationStatus = () => {
     return (
         <div>
-            <h3>RESERVATION : </h3>
-            <Row className="reservationMain-container">
-                <Row className='reservation-container'>
-                    {reservationData.map((reservation, index) => (
-                        <React.Fragment key={index}>
-                            <Col span={12}>
-                                <div className="reservation-card">
-                                    <img src={reservation.imageUrl} alt={reservation.movieTitle} />
-                                </div>
-                            </Col>
-                            <Col span={12}>
-                                <div className="reservation-details">
-                                    <Text>
-                                        Movie Title: {reservation.movieTitle}<br />
-                                        Date: {reservation.date}<br />
-                                        Location: {reservation.Location}<br />
-                                        Schedule: {reservation.schedule}<br />
-                                        Status: {reservation.status}<br />
-                                    </Text>
-                                </div>
-                            </Col>
-                        </React.Fragment>))}
+            <h3>RESERVATION:</h3>
+            {reservationData.map((reservation, index) => (
+                <Row key={index} className="reservationMain-container">
+                    <Col span={12} className="reservation-card">
+                        <img src={reservation.imageUrl} alt={reservation.movieTitle} />
+                    </Col>
+                    <Col span={12} className="reservation-details">
+                        <Text>
+                            Movie Title: {reservation.movieTitle}<br />
+                            Date: {reservation.date}<br />
+                            Location: {reservation.location}<br />
+                            Schedule: {reservation.schedule}<br />
+                            Status: {reservation.status}<br />
+                            <div className="button-container">
+                                <NavLink to="/"><br />
+                                    <Button>
+                                        Pay Now
+                                    </Button>
+                                </NavLink>
+                            </div>
+                        </Text>
+                    </Col>
+
                 </Row>
-
-            </Row>
-
+            ))}
         </div>
     );
 };
