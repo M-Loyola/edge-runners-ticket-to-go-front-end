679aeb481d7396556ed2114a4ca2cf12b5badafe
M-Loyola <m.yakit.loyola@gmail.com>
2023-09-08 02:02:37 +0800

    feat: add dummy autocomplete

--- src/components/MenuNav.js
+++ src/components/MenuNav.js
@@ -1,7 +1,6 @@
+import React, { useState } from "react";
+import { AutoComplete, Input, Menu } from "antd";
 import { EyeOutlined, HomeFilled } from "@ant-design/icons";
-import { Menu } from "antd";
-import Search from "antd/es/input/Search";
-import { useState } from "react";
 import { NavLink, useLocation, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ReactComponent as AccountIcon } from "../assets/icons/account.svg";
@@ -9,50 +8,71 @@ import { ReactComponent as LoggedInAccountIcon } from "../assets/icons/logged-in
 import "../assets/styles/MenuNav.css";
 import { setSearchInput } from "../reducers/ticketReducer";
 
-
 const MenuNav = () => {
-const signedInData = localStorage.getItem("user");
-  const menuItems = signedInData !== null ? [
-    {
-      key: "home",
-      label: "Home",
-      icon: <HomeFilled />,
-      link: "/",
-    },
-    {
-      key: "view",
-      label: "View Reservation",
-      icon: <EyeOutlined />,
-      link: "/view-reservation",
-    },
-  ] : [
-    {
-      key: "home",
-      label: "Home",
-      icon: <HomeFilled />,
-      link: "/",
-    },
-    {
-      key: "view",
-      label: "View Reservation",
-      icon: <EyeOutlined />,
-      link: "/AccountPage",
-    },
-  ];
-    const dispatch = useDispatch();
-  
+  const signedInData = localStorage.getItem("user");
+
+  const menuItems = signedInData !== null
+    ? [
+      {
+        key: "home",
+        label: "Home",
+        icon: <HomeFilled />,
+        link: "/",
+      },
+      {
+        key: "view",
+        label: "View Reservation",
+        icon: <EyeOutlined />,
+        link: "/view-reservation",
+      },
+    ]
+    : [
+      {
+        key: "home",
+        label: "Home",
+        icon: <HomeFilled />,
+        link: "/",
+      },
+      {
+        key: "view",
+        label: "View Reservation",
+        icon: <EyeOutlined />,
+        link: "/AccountPage",
+      },
+    ];
+
+  const dispatch = useDispatch();
   const location = useLocation();
   const currentPath = location.pathname;
-    const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState([]);
 
-    const handleSearchChange = (e) => {
-        setSearchValue(e.target.value);
-    };
+  const handleSearchChange = (value) => {
+    setSearchValue(value);
 
-    const onSearch = () => {
-        dispatch(setSearchInput(searchValue.trim()));
-        setSearchValue("");
-    };
+    // Example: Fetch autocomplete suggestions from an API
+    // Replace this with your own data source logic
+    // For now, let's assume you have an array of suggestions
+    const suggestions = [
+      "spider man",
+      "spider man",
+      "John Wick",
+      // Add more suggestions here
+    ];
+
+    // Filter suggestions based on the current search value
+    const filteredSuggestions = suggestions.filter((item) =>
+      item.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setAutocompleteSuggestions(filteredSuggestions);
+  };
+
+  const onSearch = () => {
+    dispatch(setSearchInput(searchValue.trim()));
+    setSearchValue("");
+  };
+
   return (
     <div className="header-container">
       <div className="menu-container">
@@ -73,16 +93,23 @@ const signedInData = localStorage.getItem("user");
         <div className="ticket">Ticket To Go</div>
       </div>
       <div className="search-container">
-        <Search
+        <AutoComplete
           placeholder="input search text"
           className="search-input"
           allowClear
-          enterButton={<button className="custom-search-button">Search</button>}
           size="large"
-                    value={searchValue}
-                    onChange={handleSearchChange}
-          onSearch={onSearch}
-        />
+          value={searchValue}
+          onChange={handleSearchChange}
+          dataSource={autocompleteSuggestions.map((item) => ({ value: item }))}
+        >
+          <Input
+            suffix={
+              <button className="custom-search-button" onClick={onSearch}>
+                Search
+              </button>
+            }
+          />
+        </AutoComplete>
         {signedInData === null ? (
           <Link to="/accountPage" className="account-link">
             <AccountIcon className="account-icon" />
