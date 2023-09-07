58944335a8b9a7077d787702277b50345f69db23
TACULMA2 <marigold.taculog@oocl.com>
2023-09-07 18:14:42 +0800

    refactor: refacotr long codes, added more css style

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,9 @@
-import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { Alert, Row, Col, Input, Button, Form } from 'antd';
 import "../assets/styles/AccountPage.css";
+import { color } from 'framer-motion';
 
 export const SignUp = () => {
-  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
@@ -22,48 +21,30 @@ export const SignUp = () => {
   const [showPasswordSpacesAlert, setShowPasswordSpacesAlert] = useState(false);
   const [showGcashNumberSpacesAlert, setShowGcashNumberSpacesAlert] = useState(false);
 
-  const handleBack = () => {
-    navigate('/');
-  };
-
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
     const trimmedValue = value.trim();
 
-    if (value !== trimmedValue) {
-        if (name === 'firstName') {
-          setShowFirstNameSpacesAlert(true);
-        } else if (name === 'lastName') {
-          setShowLastNameSpacesAlert(true);
-        } else if (name === 'email') {
-          setShowEmailSpacesAlert(true);
-        } else if (name === 'password') {
-          setShowPasswordSpacesAlert(true);
-        } else if (name === 'gcashNumber') {
-          setShowGcashNumberSpacesAlert(true);
-        }
-      } else {
-        if (name === 'firstName') {
-          setShowFirstNameSpacesAlert(false);
-        } else if (name === 'lastName') {
-          setShowLastNameSpacesAlert(false);
-        } else if (name === 'email') {
-          setShowEmailSpacesAlert(false);
-        } else if (name === 'password') {
-          setShowPasswordSpacesAlert(false);
-        } else if (name === 'gcashNumber') {
-          setShowGcashNumberSpacesAlert(false);
-        }
-      }
+    const spacesAlert = {
+        firstName: setShowFirstNameSpacesAlert,
+        lastName: setShowLastNameSpacesAlert,
+        email: setShowEmailSpacesAlert,
+        passowd: setShowPasswordSpacesAlert,
+        gcashNumber: setShowGcashNumberSpacesAlert
+    };
+
+    if (spacesAlert[name]) {
+        spacesAlert[name](value !== trimmedValue);
+    }
 
       if (name === 'email') {
         const isValidEmail = value.includes('@') && value.includes('.com');
         setShowEmailAlert(!isValidEmail);
       }
 
-      if (name == 'gcashNumber' ){
-        const isValidGcashNumber = /^\d{11}$/.test(value) && value.startsWith('0');
+      if (name === 'gcashNumber' ){
+        const isValidGcashNumber = /^\d{11}$/.test(value) && value.startsWith('09');
         setshowGcashNumberValid(!isValidGcashNumber);
       }
   };
@@ -87,7 +68,6 @@ const hasAlerts = showFirstNameSpacesAlert || showLastNameSpacesAlert || showEma
         alert('Form submitted successfully');
       }
 };
-
 
   return (
     <Form onSubmit={handleFormSubmit}>
@@ -181,8 +161,7 @@ const hasAlerts = showFirstNameSpacesAlert || showLastNameSpacesAlert || showEma
           </Form.Item>
         </Col>
       </Row>
-      <Button type="submit" className='submit-button' onClick={handleFormSubmit}>Submit</Button> 
-      <Button type="back" onClick={handleBack}>Back</Button>
+      <Button type="submit" className='submit-button' onClick={handleFormSubmit} style={{backgroundColor:"black", color:"#D2DE32"}}>Submit</Button> 
       {showEmptyFieldAlert && (
         <Alert message= "Please fill all the fields." type='error' showIcon />
       )}
