ebc568802b8923c20768d1e007978a2cd5198fba
jamillajessriel17 <76465232+jamillajessriel17@users.noreply.github.com>
2023-09-07 19:57:59 +0800

    feat: implement log out

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -1,49 +1,49 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "../assets/styles/AccountPage.css";
-import { Typography, Row, Col } from 'antd';
+import { Typography, Row, Col } from "antd";
+import { useDispatch } from "react-redux";
+import { loggedOutUser } from "../reducers/ticketReducer";
 
 const { Text } = Typography;
 
 export const UserAccount = () => {
-const navigate = useNavigate();
-    const handleBack = () => {
-        navigate('/');
-      };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleBack = () => {
+    navigate("/");
+  };
 
-      const handleLogout = () => {
-        navigate('/AccountPage'); 
-      };
+  const handleLogout = () => {
+    localStorage.clear();
+    dispatch(loggedOutUser(null));
+    navigate("/");
+  };
 
-      return (
- <div>
-    <div className="reserve-container" >
-            <Row gutter={[0, 16]}>
-                <Col span={24}>
-                    <Text className="fullname-container">
-                        Full Name 
-                    </Text>
-                </Col>
+  return (
+    <div>
+      <div className="reserve-container">
+        <Row gutter={[0, 16]}>
+          <Col span={24}>
+            <Text className="fullname-container">Full Name</Text>
+          </Col>
 
-                <Col span={24}>
-                    <Text className="contactnumber-container">
-                        Contact
-                    </Text>
-                </Col>
-                
-                <Col span={24}>
-                    <Text className="emailaddress-container">
-                        Email Address
-                    </Text>
-                </Col>
-        
-                <Col>
-                </Col>
-            </Row>
-      </div >
-            <button className="button" onClick={handleLogout}>
-             Logout
-            </button>
-            <button type="submit" className='button' onClick={handleBack}>back</button>
-       </div>
-);
+          <Col span={24}>
+            <Text className="contactnumber-container">Contact</Text>
+          </Col>
+
+          <Col span={24}>
+            <Text className="emailaddress-container">Email Address</Text>
+          </Col>
+
+          <Col></Col>
+        </Row>
+      </div>
+      <button className="button" onClick={handleLogout}>
+        Logout
+      </button>
+      <button type="submit" className="button" onClick={handleBack}>
+        back
+      </button>
+    </div>
+  );
 };
--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import "../assets/styles/AccountPage.css";
-import { Tabs } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import { SignIn } from '../components/SignIn';
-import { SignUp } from '../components/SignUp';
+import { Tabs } from "antd";
+import { useNavigate } from "react-router-dom";
+import { SignIn } from "../components/SignIn";
+import { SignUp } from "../components/SignUp";
 import { UserAccount } from "../components/UserAccount";
+import { useSelector } from "react-redux";
 
 export const AccountPage = () => {
-    const { TabPane } = Tabs;
-    const navigate = useNavigate();
-    const [isSignedIn, setIsSignedIn] = useState(false);
-
-  const onChange = (key) => {
-  };
+  const { TabPane } = Tabs;
+  const navigate = useNavigate();
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const selector = useSelector((state) => state.ticket.user);
+  const onChange = (key) => {};
 
   const handleBack = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const handleSignIn = () => {
@@ -26,31 +26,36 @@ export const AccountPage = () => {
     setIsSignedIn(false);
   };
 
-  return (
-  <div className="login-container">
-  <h1 className="banner-text-login">
-    "Unleash Your Cinematic Journey: Stream, Watch, Repeat!"
-  </h1>
+  useEffect(() => {
+    console.log(selector);
+    if (selector !== null) {
+      navigate("UserAccount");
+    }
+  });
 
-{isSignedIn ? (
+  return (
+    <div className="login-container">
+      <h1 className="banner-text-login">
+        "Unleash Your Cinematic Journey: Stream, Watch, Repeat!"
+      </h1>
+
+      {isSignedIn ? (
         <UserAccount handleSignOut={handleSignOut} />
       ) : (
-  <div className="tabs-container">
-    <Tabs defaultActiveKey="1" onChange={onChange}>
-      <TabPane tab="Log In" key="1">
-        <SignIn handleSignIn={handleSignIn} />
-      </TabPane>
-      <TabPane tab="Sign Up" key="2">
-        <SignUp />
-      </TabPane>
-    </Tabs>
-  </div>
-)}
-    <button className="back-button" onClick={handleBack}>
+        <div className="tabs-container">
+          <Tabs defaultActiveKey="1" onChange={onChange}>
+            <TabPane tab="Log In" key="1">
+              <SignIn handleSignIn={handleSignIn} />
+            </TabPane>
+            <TabPane tab="Sign Up" key="2">
+              <SignUp />
+            </TabPane>
+          </Tabs>
+        </div>
+      )}
+      <button className="back-button" onClick={handleBack}>
         Back
       </button>
     </div>
-
   );
-  };
-  
+};
