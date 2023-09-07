87918b720480ffe8ec04b7015c75cc7b3aa89ab8
Jens Jovellano <jensjovellano@gmail.com>
2023-09-07 15:58:53 +0800

    Feat: Added post and put for orders

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -9,3 +9,10 @@ export const getMoviesInCinema = () => {
 export const getMovieDetailsInCinema = (cinemaMovieId) => {
   return api.get(`/movies/${cinemaMovieId}/reservationDetails`);
 }
+export const creatNewOrder = (orderDetails) => {
+  return api.post(`/orders`, orderDetails);
+}
+
+export const updateOccupiedSeats = (cinemaMovieId, newReservedSeats) => {
+  return api.put(`/movies/${cinemaMovieId}/updateOccupiedSeats`, newReservedSeats);
+}
